23a26bfd919b16e5ae2e489d96e6863def7947c8
varun1524 <varun3273@gmail.com>
2017-10-05 23:31:07 -0700

    Different API calls added for different operations

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/doCalculate', function (req, res, next) {
+router.post('/doAdd', function (req, res, next) {
     try {
         console.log(req.body);
 
@@ -20,34 +20,70 @@ router.post('/doCalculate', function (req, res, next) {
         {
             var val1 = parseFloat(req.body.value1);
             var val2 = parseFloat(req.body.value2);
-            var operation = req.body.operation;
-            if(parseFloat(val2)===0 && req.body.operation==="/"){
-                req.body.answer=null;
-                throw ("Denominator cannot be 0.");
-            }
-            else {
-                switch (operation) {
-                    case '+':
-                        req.body.answer = val1 + val2;
-                        req.body.operationStatus = true;
-                        break;
-                    case '-':
-                        req.body.answer = val1 - val2;
-                        req.body.operationStatus = true;
-                        break;
-                    case '*':
-                        req.body.answer = val1 * val2;
-                        req.body.operationStatus = true;
-                        break;
-                    case '/':
-                        req.body.answer = val1 / val2;
-                        req.body.operationStatus = true;
-                        break;
-                    default:
-                        console.log("Error in Calculating result");
-                        break
-                }
-            }
+            req.body.answer = val1 + val2;
+            req.body.operationStatus = true;
+            res.status(200).send(req.body);
+        }
+    }
+    catch (e)
+    {
+        req.body.message = e;
+        req.body.answer=null;
+        req.body.operationStatus = false;
+        console.log(e);
+        res.status(301).send(req.body);
+    }
+    console.log("After Calculation:");
+    console.log(req.body);
+
+});
+
+router.post('/doSub', function (req, res, next) {
+    try {
+        console.log(req.body);
+
+        if(isNaN(req.body.value1) || isNaN(req.body.value2))
+        {
+            req.body.answer=null;
+            throw ("Incorrect Input Values. Please enter Numeric values only");
+        }
+        else
+        {
+            var val1 = parseFloat(req.body.value1);
+            var val2 = parseFloat(req.body.value2);
+            req.body.answer = val1 - val2;
+            req.body.operationStatus = true;
+            res.status(200).send(req.body);
+        }
+    }
+    catch (e)
+    {
+        req.body.message = e;
+        req.body.answer=null;
+        req.body.operationStatus = false;
+        console.log(e);
+        res.status(301).send(req.body);
+    }
+    console.log("After Calculation:");
+    console.log(req.body);
+});
+
+router.post('/doMul', function (req, res, next) {
+    try {
+        console.log(req.body);
+
+        if(isNaN(req.body.value1) || isNaN(req.body.value2))
+        {
+            req.body.answer=null;
+            throw ("Incorrect Input Values. Please enter Numeric values only");
+        }
+        else
+        {
+            var val1 = parseFloat(req.body.value1);
+            var val2 = parseFloat(req.body.value2);
+            req.body.answer = val1 * val2;
+            req.body.operationStatus = true;
+            res.status(200).send(req.body);
         }
     }
     catch (e)
@@ -56,12 +92,47 @@ router.post('/doCalculate', function (req, res, next) {
         req.body.answer=null;
         req.body.operationStatus = false;
         console.log(e.message);
-        console.log(e.stack);
+        res.status(301).send(req.body);
     }
     console.log("After Calculation:");
     console.log(req.body);
-    res.status(200).send(req.body);
 });
 
+router.post('/doDiv', function (req, res, next) {
+    try {
+        console.log(req.body);
+
+        if(isNaN(req.body.value1) || isNaN(req.body.value2))
+        {
+            req.body.answer=null;
+            throw ("Incorrect Input Values. Please enter Numeric values only");
+        }
+        else
+        {
+            var val1 = parseFloat(req.body.value1);
+            var val2 = parseFloat(req.body.value2);
+            if(val2===0 && req.body.operation==="/"){
+                req.body.answer=null;
+                throw ("Denominator cannot be 0.");
+            }
+            else {
+                req.body.answer = val1 / val2;
+                req.body.operationStatus = true;
+                res.status(200).send(req.body);
+
+            }
+        }
+    }
+    catch (e)
+    {
+        req.body.message = e;
+        req.body.answer=null;
+        req.body.operationStatus = false;
+        console.log(e);
+        res.status(301).send(req.body);
+    }
+    console.log("After Calculation:");
+    console.log(req.body);
+});
 
 module.exports = router;
